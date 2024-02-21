f6ef3cf0752771a2680422ef9d6bb11514ea8e43
ShivanshuPrajapati212 <shivanshuprajapati212@gmail.com>
2024-02-21 20:39:58 +0530

    Update main.py

--- Program 6/main.py
+++ Program 6/main.py
@@ -1,1 +1,21 @@
 # Lets Code Our Program 6. Enjoy!!!
+
+import random
+
+words = ["Wow","Shivanshu","Himanshu","Noob","God","Bot"]
+
+print(f"Hint: {words}")
+word = random.choice(words)
+guess = 1
+
+while True:
+    userint = input("Enter the Word : ")
+
+    if userint == word.lower():
+        print("You Guessed the word!!!")
+        print(f"The word was {word}")
+        print(f"You Guessed it in {guess} Tries.")
+        break
+    else:
+        print("Try again....")
+        guess = guess + 1
